970d43a78ba4efb000ec8f18657ce4eea03ba468
System Administrator <root@apple.local>
2015-12-10 00:44:08 +0800

    add ex6-8

--- learnPythonTheHardWay/ex5.py
+++ learnPythonTheHardWay/ex5.py
@@ -1,18 +1,18 @@
 # -*- coding:utf-8 -*-
 # Conversion	Meaning	Notes
-# d	Signed integer decimal.	
-# i	Signed integer decimal.	
+# d	Signed integer decimal.
+# i	Signed integer decimal.
 # o	Unsigned octal.	(1)
-# u	Unsigned decimal.	
+# u	Unsigned decimal.
 # x	Unsigned hexadecimal (lowercase).	(2)
 # X	Unsigned hexadecimal (uppercase).	(2)
-# e	Floating point exponential format (lowercase).	
-# E	Floating point exponential format (uppercase).	
-# f	Floating point decimal format.	
-# F	Floating point decimal format.	
-# g	Same as "e" if exponent is greater than -4 or less than precision, "f" otherwise.	
-# G	Same as "E" if exponent is greater than -4 or less than precision, "F" otherwise.	
-# c	Single character (accepts integer or single character string).	
+# e	Floating point exponential format (lowercase).
+# E	Floating point exponential format (uppercase).
+# f	Floating point decimal format.
+# F	Floating point decimal format.
+# g	Same as "e" if exponent is greater than -4 or less than precision, "f" otherwise.
+# G	Same as "E" if exponent is greater than -4 or less than precision, "F" otherwise.
+# c	Single character (accepts integer or single character string).
 # r	String (converts any python object using repr()).	(3)
 # s	String (converts any python object using str()).	(4)
 # %	No argument is converted, results in a "%" character in the result.
