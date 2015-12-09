1e680d75e617d3a3c53ab501c74f032518026893
unknown <yangjiao@FX-JSJ991.funshion.com>
2015-12-09 18:27:23 +0800

    add learn python the hard way ex1-5

---
+++ learnPythonTheHardWay/ex1.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+print "Hello World!"
+print "Hello Again"
+# print "I like typing this."
+print "This is fun."
+#不换行符，后边加 ,
+print 'Yay! Printing.',
+#换行符，后边加 \n
+print "I'd much rather you 'not'.\n"
+print 'I "said" do not touch this.'
