4786cd4acd1c361445828c4e1a8082cc8665a9cc
yangjiao <yangjiao91@gmail.com>
2015-12-20 19:26:55 +0800

    add ex28.py:Boolean Practice

---
+++ learnPythonTheHardWay/ex28.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+True and True  #True
+False and True  # False
+1 == 1 and 2 == 1  #Flase
+"test" == "test"  #True
+1 == 1 or 2 != 1  #True
+True and 1 == 1    #True
+False and 0 != 0    #False
+True or 1 == 1       #True
+"test" == "testing"   #False
+1 != 0 and 2 == 1     #False
+"test" != "testing"    #True
+"test" == 1            #False
+not (True and False)   #True
+not (1 == 1 and 0 != 1)   #False
+not (10 == 1 or 1000 == 1000)  #False
+not (1 != 10 or 3 == 4)    #False
+not ("testing" == "testing" and "Zed" == "Cool Guy")    #True
+1 == 1 and (not ("testing" == 1 or 1 == 0))     #True
+"chunky" == "bacon" and (not (3 == 4 or 3 == 3))  #False
+3 == 3 and (not ("testing" == "testing" or "Python" == "Fun"))  #False
+
+
+# ??????
+# >>> 1 and True
+# True
+# >>> True and 1
+# 1
+# >>> False and 0
+# False
+# >>> 0 and False
+# 0
