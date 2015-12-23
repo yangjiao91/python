cd3ea6755b6301408148e99e01554442c4535402
yangjiao91 <yangjiao91@gmail.com>
2015-12-23 17:48:37 +0800

    modify the way to open file

--- learnPythonTheHardWay/ex15.py
+++ learnPythonTheHardWay/ex15.py
@@ -2,11 +2,20 @@
 from sys import argv
 
 script,filename = argv
-txt = open(filename)
 
-print "Here's your file %r:" % filename
-print txt.read()
-txt.close()
+# the right way to open file
+try:
+    txt = open(filename)
+except:
+    print 'fail to open'
+    exit(1)
+try:
+    print "Here's your file %r:" % filename
+    print txt.read()
+except:
+    print 'fail to read'
+finally:
+    txt.close()
 
 print "Type the filename again:"
 file_again = raw_input("> ")
