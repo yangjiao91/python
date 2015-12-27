3771f39a5fe8e40aee05acdae4caf30cae67824e
yangjiao <yangjiao91@gmail.com>
2015-12-27 18:32:44 +0800

    dict learn

---
+++ learnPythonTheHardWay/resource/ex39_orderDict.py
@@ -0,0 +1,14 @@
+#-*- coding:utf-8 -*-
+import collections
+
+d1 = {}
+d1['a'] = 'A'
+d1['b'] = 'B'
+d1['c'] = 'C'
+print "d1: ",d1
+
+d2 = collections.OrderedDict()
+d2['a'] = 'A'
+d2['b'] = 'B'
+d2['c'] = 'C'
+print "d2: ",d2
