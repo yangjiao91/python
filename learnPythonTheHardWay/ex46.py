c53a376ba2e8b2b926dc6ace8070a46df01e9351
yangjiao91 <yangjiao91@gmail.com>
2016-01-11 18:34:14 +0800

    nosetests and project skeleton

--- learnPythonTheHardWay/ex46.py
+++ learnPythonTheHardWay/ex46.py
@@ -3,3 +3,20 @@
 # distribute from http://pypi.python.org/pypi/distribute
 # nose from http://pypi.python.org/pypi/nose/
 # virtualenv from http://pypi.python.org/pypi/virtualenv
+
+from nose.tools import *
+
+def setup():
+    print "function setup"
+
+def TestFunc1():
+    print "TestFunc1"
+    assert True
+
+def tearDown():
+    print "function tearDown"
+
+@nottest
+def TestFunc2():
+    print "TestFunc2"
+    assert True
