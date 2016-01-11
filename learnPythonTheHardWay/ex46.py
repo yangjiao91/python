f02d3acc7095ef7ee21acfa4c2b6aefd12892629
yangjiao <yangjiao91@gmail.com>
2016-01-11 13:03:03 +0800

    a project

---
+++ learnPythonTheHardWay/ex46.py
@@ -0,0 +1,5 @@
+# -*- coding:utf-8 -*-
+# pip from http://pypi.python.org/pypi/pip
+# distribute from http://pypi.python.org/pypi/distribute
+# nose from http://pypi.python.org/pypi/nose/
+# virtualenv from http://pypi.python.org/pypi/virtualenv
---
+++ learnPythonTheHardWay/test/testNose.py
@@ -0,0 +1,13 @@
+def setup():
+    print "function setup"
+
+def TestFunc1():
+    print "TestFunc1"
+    assert True
+
+def TestFunc2():
+    print "TestFunc2"
+    assert True
+
+def tearDown():
+    print "function tearDown"
