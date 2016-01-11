63df60e6522ff4cd9952f795ad95bbf6079852eb
yangjiao <yangjiao91@gmail.com>
2016-01-12 00:31:54 +0800

    auto test

---
+++ learnPythonTheHardWay/ex47/setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except importError:
+    from distutils.core import setup
+
+config = {
+    'description': 'My Project',
+    'author': 'yang jiao',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'My email.',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['ex47'],
+    'scripts': [],
+    'name': 'projectname'
+}
+
+setup(**config)
